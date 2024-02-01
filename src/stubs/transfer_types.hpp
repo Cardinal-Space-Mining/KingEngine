a4225fd8661be5154583d753b93e16aa4d023022
wimos-ai <84042085+wimos-ai@users.noreply.github.com>
2024-02-01 17:41:45 -0600

    Update transfer_types.hpp

--- src/stubs/transfer_types.hpp
+++ src/stubs/transfer_types.hpp
@@ -8,6 +8,9 @@ typedef struct locationF_t{
 }locationF_t;
 
 namespace KingEngine{
+
+
+    constexpr int32_t SCALE_FACTOR = 100000;
     //Location of robot. Units are in mm. For internal use only
     typedef struct locationI_t{
         int32_t x_mm;
@@ -17,12 +20,12 @@ namespace KingEngine{
     inline locationF_t from_integer_location(locationI_t local){
         float x_mm_f = local.x_mm;
         float y_mm_f = local.y_mm;
-        return {x_mm_f / 1000.0f, y_mm_f / 1000.0f};
+        return {x_mm_f / SCALE_FACTOR, y_mm_f / SCALE_FACTOR};
     }
 
     inline locationI_t from_float_location(locationF_t local){
-        int32_t x_m_i = local.x_m * 1000;
-        int32_t y_m_i = local.y_m * 1000;
+        int32_t x_m_i = local.x_m * SCALE_FACTOR;
+        int32_t y_m_i = local.y_m * SCALE_FACTOR;
         return {x_m_i, y_m_i};
     }
 }
