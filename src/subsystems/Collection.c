9d20bb48651cbb18192889e4b2a265ce914d5f4d
Gavin Fisher <gdf73278@iastate.edu>
2024-02-17 13:16:04 -0600

    changing collection and hopper to c++

--- src/subsystems/Collection.c
+++
@@ -1,5 +0,0 @@
-// This class is for the mining segment
-// Sets a speed
-// Starts based on Hopper position
-// Algorithm to keep track of resistance in belt
-// Timer
---
+++ src/subsystems/Collection.cpp
@@ -0,0 +1,20 @@
+
+
+// This class is for the mining segment
+// Sets a speed
+// Starts based on Hopper position
+// Algorithm to keep track of resistance in belt
+// Timer
+
+
+// imports
+
+class Collection {
+    private:
+        double trencherEncoder = 0;
+        double trencherSpeed = 0;
+        double trencherCurrent = 0;
+        double trencherVoltage = 0;
+        double busVoltage = 0;
+};
+// trench variables
