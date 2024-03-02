5b49082ad9acaf8f9e900367dfd66285d1aa05f9
Sam Richter <samrichter888@gmail.com>
2024-03-01 23:43:36 -0600

    add pose struct to cmake, refine lidar api, submodule accumulator integrated

--- src/ros/lidar/src/lidar.cpp
+++ src/ros/lidar/src/lidar.cpp
@@ -2,19 +2,32 @@
 #include "lidar_api.h"
 
 
-void ros_bridge::on_pose_update(const custom_types::msg::Pose &pose) {
-    const float
-        _xyz[3] = { pose.x, pose.y, pose.z },
-        _quat[4] = { pose.qx, pose.qy, pose.qz, pose.qw };
-    ldrp::updateWorldPose(_xyz, _quat, 0);
+void ros_bridge::on_pose_update(const custom_types::msg::Pose& pose) {
+
+    const float _pose[7] =
+    {
+        (float)pose.x,
+        (float)pose.y,
+        (float)pose.z,
+        (float)pose.qx,
+        (float)pose.qy,
+        (float)pose.qz,
+        (float)pose.qw
+    };
+
+    ldrp::updateWorldPose(_pose, _pose + 3);     // << timestamp!?
+
 }
 
 void ros_bridge::on_startup() {
+
     ldrp::apiInit();
     ldrp::lidarInit();
-}
 
+}
 void ros_bridge::on_shutdown() {    // may block since this waits for all threads to join
+
     ldrp::lidarShutdown();
     ldrp::apiDestroy();
+
 }
