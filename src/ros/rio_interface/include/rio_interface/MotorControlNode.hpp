53d901f0f59bb3fba710a2002511fad531cb7778
wimos-ai <willmoiser@gmail.com>
2024-03-02 16:48:15 -0600

    Added ros node for motor control.

---
+++ src/ros/rio_interface/include/rio_interface/MotorControlNode.hpp
@@ -0,0 +1,26 @@
+#include <mutex>
+
+#include "rclcpp/rclcpp.hpp"
+
+#include "custom_types/msg/motor_neutral.hpp"
+#include "custom_types/msg/motor_percent.hpp"
+#include "custom_types/msg/motor_velocity.hpp"
+#include "SerialMotorCtrl.h"
+
+class MotorCtrlNode : public rclcpp::Node
+{
+public:
+    MotorCtrlNode(std::mutex &locking_mtx, MotorSerialConnection &rio_conn);
+
+private:
+    void neutral_cb(const custom_types::msg::MotorNeutral& msg);
+    void percent_cb(const custom_types::msg::MotorPercent& msg);
+    void velocity_cb(const custom_types::msg::MotorVelocity& msg);
+
+    std::mutex &locking_mtx;
+    MotorSerialConnection &rio_conn;
+
+    rclcpp::Subscription<custom_types::msg::MotorNeutral>::SharedPtr neutral_sub;
+    rclcpp::Subscription<custom_types::msg::MotorPercent>::SharedPtr percent_sub;
+    rclcpp::Subscription<custom_types::msg::MotorVelocity>::SharedPtr velocity_sub;
+};
---
+++ src/ros/rio_interface/src/MotorControlNode.cpp
@@ -0,0 +1,39 @@
+#include "rio_interface/MotorControlNode.hpp"
+#include <functional>
+
+using custom_types::msg::MotorNeutral;
+using custom_types::msg::MotorPercent;
+using custom_types::msg::MotorVelocity;
+using std::placeholders::_1;
+
+MotorCtrlNode::MotorCtrlNode(std::mutex &locking_mtx, MotorSerialConnection &rio_conn) : Node("motor_connector"), locking_mtx(locking_mtx), rio_conn(rio_conn)
+{
+    this->neutral_sub = this->create_subscription<MotorNeutral>("location", 10, std::bind(&MotorCtrlNode::neutral_cb, this, _1));
+    this->percent_sub = this->create_subscription<MotorPercent>("location", 10, std::bind(&MotorCtrlNode::percent_cb, this, _1));
+    this->velocity_sub = this->create_subscription<MotorVelocity>("location", 10, std::bind(&MotorCtrlNode::velocity_cb, this, _1));
+}
+
+void MotorCtrlNode::neutral_cb(const MotorNeutral &msg)
+{
+    std::lock_guard(this->locking_mtx);
+    if (msg.coast_t_or_break_f == true)
+    {
+        this->rio_conn.set_neutral_mode(msg.motor_id, MotorNeutralMode::MOTOR_COAST);
+    }
+    else
+    {
+        this->rio_conn.set_neutral_mode(msg.motor_id, MotorNeutralMode::MOTOR_BREAK);
+    }
+}
+
+void MotorCtrlNode::percent_cb(const custom_types::msg::MotorPercent &msg)
+{
+    std::lock_guard(this->locking_mtx);
+    this->rio_conn.set_percent(msg.motor_id, msg.motor_percent);
+}
+
+void MotorCtrlNode::velocity_cb(const custom_types::msg::MotorVelocity &msg)
+{
+    std::lock_guard(this->locking_mtx);
+    this->rio_conn.set_percent(msg.motor_id, msg.velocity_turns_per_second);
+}
