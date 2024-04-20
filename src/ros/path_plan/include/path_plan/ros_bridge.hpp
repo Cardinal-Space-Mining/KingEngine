7986fb5e510e68f45e0bbb4a883cb8c4ee89c214
Connor Tynan <connortynan16@gmail.com>
2024-04-20 17:15:33 -0500

    Change map msgs to OccupancyGrids

--- src/ros/path_plan/include/path_plan/ros_bridge.hpp
+++ src/ros/path_plan/include/path_plan/ros_bridge.hpp
@@ -1,7 +1,12 @@
+#pragma once
+
 #include <vector>
 #include <optional>
 #include <utility>
 #include <cstdint>
+
+#include "rclcpp/rclcpp.hpp"
+#include "nav_msgs/msg/occupancy_grid.hpp"
 
 #include "custom_types/msg/map.hpp"
 
@@ -13,10 +18,15 @@ namespace ros_bridge
     using optional_point = std::optional<point>;
     using optional_path = std::optional<path>;
 
-    optional_path on_lidar_data(const custom_types::msg::Map& map);
+    void map_init();
+
+    optional_path on_lidar_data(const nav_msgs::msg::OccupancyGrid& msg);
 
     optional_path on_location_change(double x, double y);
     optional_path on_destination_change(double x, double y);
+
+    nav_msgs::msg::OccupancyGrid get_grid_values();
+
 } // namespace ros_bridge
 
 
