cc4d702bd983cd4110494b4407798b8d4e9f4dc0
timkuehn <timkuehn@iastate.edu>
2024-04-27 11:05:25 -0500

    smol changes

--- src/ros/king_engine/autonomy/auto.cpp
+++ src/ros/king_engine/autonomy/auto.cpp
@@ -1,4 +1,16 @@
 #include "./auto.hpp"
 AutonomyNode::AutonomyNode() : rclcpp::Node("autonomy_node") 
 {
+
+}
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  node = std::make_shared<AutonomyNode>();
+  auto timer = node->create_timer(std::chrono::seconds(0.05), update);
+  rclcpp::spin(node);
+  node = nullptr;
+  rclcpp::shutdown();
+  return 0;
 }
