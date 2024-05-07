30b46ff50b9d0f4da008bc4bd377c9ea5cdeed2d
Jacob M <willmoiser@gmail.com>
2024-05-07 15:09:28 -0500

    Merge remote-tracking branch 'origin/main' into system_integration_2. Readded path planning ros2 configuration parameters.

--- src/ros/path_plan/src/path_plan/pathplan.cpp
+++ src/ros/path_plan/src/path_plan/pathplan.cpp
@@ -1,41 +1,41 @@
 /*******************************************************************************
-*   Copyright (C) 2024 Cardinal Space Mining Club                              *
-*                                                                              *
-*   Unless required by applicable law or agreed to in writing, software        *
-*   distributed under the License is distributed on an "AS IS" BASIS,          *
-*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
-*   See the License for the specific language governing permissions and        *
-*   limitations under the License.                                             *
-*                                                                              *
-*                                ;xxxxxxx:                                     *
-*                               ;$$$$$$$$$       ...::..                       *
-*                               $$$$$$$$$$x   .:::::::::::..                   *
-*                            x$$$$$$$$$$$$$$::::::::::::::::.                  *
-*                        :$$$$$&X;      .xX:::::::::::::.::...                 *
-*                .$$Xx++$$$$+  :::.     :;:   .::::::.  ....  :                *
-*               :$$$$$$$$$  ;:      ;xXXXXXXXx  .::.  .::::. .:.               *
-*              :$$$$$$$$: ;      ;xXXXXXXXXXXXXx: ..::::::  .::.               *
-*             ;$$$$$$$$ ::   :;XXXXXXXXXXXXXXXXXX+ .::::.  .:::                *
-*              X$$$$$X : +XXXXXXXXXXXXXXXXXXXXXXXX; .::  .::::.                *
-*               .$$$$ :xXXXXXXXXXXXXXXXXXXXXXXXXXXX.   .:::::.                 *
-*                X$$X XXXXXXXXXXXXXXXXXXXXXXXXXXXXx:  .::::.                   *
-*                $$$:.XXXXXXXXXXXXXXXXXXXXXXXXXXX  ;; ..:.                     *
-*                $$& :XXXXXXXXXXXXXXXXXXXXXXXX;  +XX; X$$;                     *
-*                $$$::XXXXXXXXXXXXXXXXXXXXXX: :XXXXX; X$$;                     *
-*                X$$X XXXXXXXXXXXXXXXXXXX; .+XXXXXXX; $$$                      *
-*                $$$$ ;XXXXXXXXXXXXXXX+  +XXXXXXXXx+ X$$$+                     *
-*              x$$$$$X ;XXXXXXXXXXX+ :xXXXXXXXX+   .;$$$$$$                    *
-*             +$$$$$$$$ ;XXXXXXx;;+XXXXXXXXX+    : +$$$$$$$$                   *
-*              +$$$$$$$$: xXXXXXXXXXXXXXX+      ; X$$$$$$$$                    *
-*               :$$$$$$$$$. +XXXXXXXXX:      ;: x$$$$$$$$$                     *
-*               ;x$$$$XX$$$$+ .;+X+      :;: :$$$$$xX$$$X                      *
-*              ;;;;;;;;;;X$$$$$$$+      :X$$$$$$&.                             *
-*              ;;;;;;;:;;;;;x$$$$$$$$$$$$$$$$x.                                *
-*              :;;;;;;;;;;;;.  :$$$$$$$$$$X                                    *
-*               .;;;;;;;;:;;    +$$$$$$$$$                                     *
-*                 .;;;;;;.       X$$$$$$$:                                     *
-*                                                                              *
-*******************************************************************************/
+ *   Copyright (C) 2024 Cardinal Space Mining Club                              *
+ *                                                                              *
+ *   Unless required by applicable law or agreed to in writing, software        *
+ *   distributed under the License is distributed on an "AS IS" BASIS,          *
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+ *   See the License for the specific language governing permissions and        *
+ *   limitations under the License.                                             *
+ *                                                                              *
+ *                                ;xxxxxxx:                                     *
+ *                               ;$$$$$$$$$       ...::..                       *
+ *                               $$$$$$$$$$x   .:::::::::::..                   *
+ *                            x$$$$$$$$$$$$$$::::::::::::::::.                  *
+ *                        :$$$$$&X;      .xX:::::::::::::.::...                 *
+ *                .$$Xx++$$$$+  :::.     :;:   .::::::.  ....  :                *
+ *               :$$$$$$$$$  ;:      ;xXXXXXXXx  .::.  .::::. .:.               *
+ *              :$$$$$$$$: ;      ;xXXXXXXXXXXXXx: ..::::::  .::.               *
+ *             ;$$$$$$$$ ::   :;XXXXXXXXXXXXXXXXXX+ .::::.  .:::                *
+ *              X$$$$$X : +XXXXXXXXXXXXXXXXXXXXXXXX; .::  .::::.                *
+ *               .$$$$ :xXXXXXXXXXXXXXXXXXXXXXXXXXXX.   .:::::.                 *
+ *                X$$X XXXXXXXXXXXXXXXXXXXXXXXXXXXXx:  .::::.                   *
+ *                $$$:.XXXXXXXXXXXXXXXXXXXXXXXXXXX  ;; ..:.                     *
+ *                $$& :XXXXXXXXXXXXXXXXXXXXXXXX;  +XX; X$$;                     *
+ *                $$$::XXXXXXXXXXXXXXXXXXXXXX: :XXXXX; X$$;                     *
+ *                X$$X XXXXXXXXXXXXXXXXXXX; .+XXXXXXX; $$$                      *
+ *                $$$$ ;XXXXXXXXXXXXXXX+  +XXXXXXXXx+ X$$$+                     *
+ *              x$$$$$X ;XXXXXXXXXXX+ :xXXXXXXXX+   .;$$$$$$                    *
+ *             +$$$$$$$$ ;XXXXXXx;;+XXXXXXXXX+    : +$$$$$$$$                   *
+ *              +$$$$$$$$: xXXXXXXXXXXXXXX+      ; X$$$$$$$$                    *
+ *               :$$$$$$$$$. +XXXXXXXXX:      ;: x$$$$$$$$$                     *
+ *               ;x$$$$XX$$$$+ .;+X+      :;: :$$$$$xX$$$X                      *
+ *              ;;;;;;;;;;X$$$$$$$+      :X$$$$$$&.                             *
+ *              ;;;;;;;:;;;;;x$$$$$$$$$$$$$$$$x.                                *
+ *              :;;;;;;;;;;;;.  :$$$$$$$$$$X                                    *
+ *               .;;;;;;;;:;;    +$$$$$$$$$                                     *
+ *                 .;;;;;;.       X$$$$$$$:                                     *
+ *                                                                              *
+ *******************************************************************************/
 
 #include "pathplan.hpp"
 
@@ -53,36 +53,22 @@
 #include <opencv2/imgproc.hpp>
 #endif
 
+const std::string PathPlanNode::ROBOT_WIDTH_PARAM_NAME = "robot_width";
+const std::string PathPlanNode::TURN_COST_PARAM_NAME = "turn_cost";
+const std::string PathPlanNode::MIN_WEIGHT_PARAM_NAME = "min_weight";
 
-PathPlanNode::PathPlanNode(
-	float robot_width_m,
-	int turn_cost,
-	int min_weight
-) :
-	Node("path_plan"),
-	robot_width{robot_width_m},
-	turn_cost{turn_cost},
-	min_weight{min_weight}
+PathPlanNode::PathPlanNode() : Node("path_plan"),
+					  lidar_data_sub(this->create_subscription<nav_msgs::msg::OccupancyGrid>("/ldrp/obstacle_grid", 1, std::bind(&PathPlanNode::lidar_change_cb, this, std::placeholders::_1))),
+					  dest_sub(this->create_subscription<geometry_msgs::msg::PoseStamped>("/destination", 1, std::bind(&PathPlanNode::destination_change_cb, this, std::placeholders::_1))),
+					  location_sub(this->create_subscription<geometry_msgs::msg::PoseStamped>("/uesim/pose", 1, std::bind(&PathPlanNode::location_change_cb, this, std::placeholders::_1))),
+					  path_pub(this->create_publisher<nav_msgs::msg::Path>("/pathplan/nav_path", 1)),
+					  weight_map_pub(this->create_publisher<nav_msgs::msg::OccupancyGrid>("/pathplan/nav_map", 1)),
+					  param_init(this->init_parameters()),
+					  robot_width(this->get_parameter(ROBOT_WIDTH_PARAM_NAME).as_double()),
+					  turn_cost(this->get_parameter(TURN_COST_PARAM_NAME).as_int()),
+					  min_weight(this->get_parameter(MIN_WEIGHT_PARAM_NAME).as_int())
 {
 	RCLCPP_INFO(this->get_logger(), "PathPlan Node Initialization!");
-
-	// subscribers
-	lidar_data_sub = this->create_subscription<nav_msgs::msg::OccupancyGrid>(
-		"/ldrp/obstacle_grid", 1,
-		std::bind(&PathPlanNode::lidar_change_cb, this, std::placeholders::_1)
-	);
-	location_sub = this->create_subscription<geometry_msgs::msg::PoseStamped>(
-		"/uesim/pose", 1,
-		std::bind(&PathPlanNode::location_change_cb, this, std::placeholders::_1)
-	);
-	dest_sub = this->create_subscription<geometry_msgs::msg::PoseStamped>(
-		"/destination", 1,
-		std::bind(&PathPlanNode::destination_change_cb, this, std::placeholders::_1)
-	);
-
-	// publishers
-	path_pub = this->create_publisher<nav_msgs::msg::Path>( "/pathplan/nav_path", 1 );
-	weight_map_pub = this->create_publisher<nav_msgs::msg::OccupancyGrid>( "/pathplan/nav_map", 1 );
 
 	this->target_pose.position.x = 11.5;
 	this->target_pose.position.y = -4.2;
@@ -90,12 +76,11 @@ PathPlanNode::PathPlanNode(
 	// add params? (for turn cost, robot width)
 }
 
-
-
-void PathPlanNode::lidar_change_cb(const nav_msgs::msg::OccupancyGrid& map) {
+void PathPlanNode::lidar_change_cb(const nav_msgs::msg::OccupancyGrid &map)
+{
 	// RCLCPP_INFO(this->get_logger(), "Recieved lidar data");
 
-	this->weights = map;	// copy occupancy grid directly since we can simply replace our old data
+	this->weights = map; // copy occupancy grid directly since we can simply replace our old data
 
 #ifdef HAVE_OPENCV
 
@@ -105,36 +90,32 @@ void PathPlanNode::lidar_change_cb(const nav_msgs::msg::OccupancyGrid& map) {
 		_w = static_cast<int>(this->weights.info.width),
 		_h = static_cast<int>(this->weights.info.height);
 
-	cv::Mat _data{ _h, _w, CV_8UC1, this->weights.data.data() };
-	cv::Mat _dest = cv::Mat::zeros(cv::Size{ _w, _h }, CV_8UC1);
+	cv::Mat _data{_h, _w, CV_8UC1, this->weights.data.data()};
+	cv::Mat _dest = cv::Mat::zeros(cv::Size{_w, _h}, CV_8UC1);
 
 	const int
-		kernel_diam = static_cast<int>(this->robot_width / this->weights.info.resolution) + 1;	// actual computation requires half of robot width / res * 2 for diam, but the 0.5 and 2 cancel
+		kernel_diam = static_cast<int>(this->robot_width / this->weights.info.resolution) + 1; // actual computation requires half of robot width / res * 2 for diam, but the 0.5 and 2 cancel
 	const cv::Size
-		kernel_size{ kernel_diam, kernel_diam };
+		kernel_size{kernel_diam, kernel_diam};
 
 	cv::dilate(
 		_data,
 		_dest,
-		cv::getStructuringElement(cv::MORPH_ELLIPSE, kernel_size)
-	);
+		cv::getStructuringElement(cv::MORPH_ELLIPSE, kernel_size));
 	cv::GaussianBlur(
 		_dest,
 		_data,
 		kernel_size,
 		10, 10,
-		cv::BorderTypes::BORDER_CONSTANT
-	);
+		cv::BorderTypes::BORDER_CONSTANT);
 	cv::max(
 		_data,
 		cv::Scalar::all(this->min_weight),
-		_data
-	);
+		_data);
 
 	RCLCPP_INFO(this->get_logger(),
-		"Weightmap globulization operation completed in %f seconds.",
-		std::chrono::duration<double>(std::chrono::high_resolution_clock::now() - _t).count()
-	);
+				"Weightmap globulization operation completed in %f seconds.",
+				std::chrono::duration<double>(std::chrono::high_resolution_clock::now() - _t).count());
 
 #else
 
@@ -145,30 +126,30 @@ void PathPlanNode::lidar_change_cb(const nav_msgs::msg::OccupancyGrid& map) {
 	this->weight_map_pub->publish(this->weights);
 
 	this->recalc_path_and_export();
-
 }
 
-void PathPlanNode::location_change_cb(const geometry_msgs::msg::PoseStamped& msg) {
+void PathPlanNode::location_change_cb(const geometry_msgs::msg::PoseStamped &msg)
+{
 
 	this->current_pose = msg.pose;
 
 	// RCLCPP_INFO(this->get_logger(), "Recieved new location: (%F, %F)", this->current_pose.position.x, this->current_pose.position.y);
 
 	// this->recalc_path_and_export();	// don't need to recalc since we are likely following the path (also lidar will be updating this as well)
-
 }
 
-void PathPlanNode::destination_change_cb(const geometry_msgs::msg::PoseStamped& msg) {
+void PathPlanNode::destination_change_cb(const geometry_msgs::msg::PoseStamped &msg)
+{
 
 	this->target_pose = msg.pose;
 
 	// RCLCPP_INFO(this->get_logger(), "Recieved new destination: (%F, %F)", this->target_pose.position.x, this->target_pose.position.y);
 
 	this->recalc_path_and_export();
-
 }
 
-void PathPlanNode::recalc_path_and_export() {
+void PathPlanNode::recalc_path_and_export()
+{
 
 	nav_msgs::msg::Path ros_path{};
 
@@ -176,7 +157,7 @@ void PathPlanNode::recalc_path_and_export() {
 
 	std::vector<Eigen::Vector2<int64_t>>
 		path = this->nav_map.navigate<uint8_t, false>(
-			reinterpret_cast<uint8_t*>(this->weights.data.data()),
+			reinterpret_cast<uint8_t *>(this->weights.data.data()),
 			this->weights.info.width,
 			this->weights.info.height,
 			this->weights.info.origin.position.x,
@@ -186,13 +167,13 @@ void PathPlanNode::recalc_path_and_export() {
 			this->current_pose.position.y,
 			this->target_pose.position.x,
 			this->target_pose.position.y,
-			this->turn_cost
-		);
+			this->turn_cost);
 
 	ros_path.poses.resize(path.size());
 	ros_path.header.stamp = this->get_clock()->now();
 	ros_path.header.frame_id = "world";
-	for(size_t i = 0; i < path.size(); i++) {
+	for (size_t i = 0; i < path.size(); i++)
+	{
 		ros_path.poses[i].pose.position.x = path[i].x() * this->weights.info.resolution + this->weights.info.origin.position.x;
 		ros_path.poses[i].pose.position.y = path[i].y() * this->weights.info.resolution + this->weights.info.origin.position.y;
 		ros_path.poses[i].pose.position.z = 0.0;
@@ -204,14 +185,21 @@ void PathPlanNode::recalc_path_and_export() {
 		ros_path.poses[i].header.frame_id = "world";
 	}
 
-	if(path.size() > 0) {
+	if (path.size() > 0)
+	{
 		RCLCPP_INFO(this->get_logger(),
-			"Successfully recalculated path in %f seconds with %ld segments.",
-			std::chrono::duration<double>(std::chrono::high_resolution_clock::now() - _t).count(),
-			path.size()
-		);
+					"Successfully recalculated path in %f seconds with %ld segments.",
+					std::chrono::duration<double>(std::chrono::high_resolution_clock::now() - _t).count(),
+					path.size());
 	}
 
 	this->path_pub->publish(ros_path);
+}
 
+bool PathPlanNode::init_parameters()
+{
+	this->declare_parameter(ROBOT_WIDTH_PARAM_NAME, DEFAULT_ROBOT_WIDTH);
+	this->declare_parameter(TURN_COST_PARAM_NAME, DEFAULT_TURN_COST);
+	this->declare_parameter(MIN_WEIGHT_PARAM_NAME, DEFAULT_MIN_WEIGHT);
+	return true;
 }
