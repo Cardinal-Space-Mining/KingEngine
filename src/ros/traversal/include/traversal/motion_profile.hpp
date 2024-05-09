3a87cc1511c16fc00b3c20356a7271823a6b975f
ErikSandberg02 <eriksandberg@comcast.net>
2024-05-09 00:06:48 -0500

    traversal stuff

--- src/ros/traversal/include/traversal/motion_profile.hpp
+++ src/ros/traversal/include/traversal/motion_profile.hpp
@@ -1,6 +1,5 @@
 #pragma once
-// STL includes
-//#include "path_plan/WeightMap.hpp"
+
 #include <ostream>	  //std::ostream
 #include <stdint.h>	  //uint16_t, uint_fast16_t
 #include <string>	  //std::string
@@ -8,128 +7,81 @@
 #include <utility>	  //std::pair
 #include <queue>	  //std::queue
 #include <functional> //std::reference_wrapper
-#include <vector>
-#include <stdint.h>	  //uint16_t, uint_fast16_t
 
+// Forward declaration
+class motion_node;
 
-using path_t = std::vector<point>;
+struct point {
+	double x, y;
 
-using motion_path = std::vector<motion_node>;
-
-//using point = WeightMap::point_t;
-using point = std::pair<uint16_t, uint16_t>;
-
-
-
-class motion_node {
-
-	motion_node(const point& a, const point& b) : a(a), b(b) {}
-
-	motion_node() {}
-
-	~motion_node() {}
-
-public:
-
-	virtual double get_tangent_angle(point p) = 0;
-
-	virtual point get_closest_point(point current) = 0;
-
-private:
-
-	point a;
-
-	point b;
-
-public:
-
-	bool operator==(const motion_node& other) const
-	{
-		return a == other.a && b == other.b;
+	bool operator==(const point& b) const {
+		return ((x > b.x - 0.01 && x < b.x + 0.01) && (y > b.y - 0.01 && y < b.y + 0.01));
 	}
-	
+
+	point(double x, double y) : x(x), y(y) {};
 };
 
+class motion_node {
+public:
+	virtual double get_tangent_angle(point p) = 0;
+	virtual point get_closest_point(point current) = 0;
+	virtual point get_target_from_distance(point current, double distance) = 0;
 
-class linear_motion : class motion_node {
+	point get_start() { return a; }
+	point get_end() { return b; }
 
-	linear_motion(point a, point b) : motion_node(a, b);
+	bool operator==(const motion_node& other) const {
+		return a == other.a && b == other.b;
+	}
 
+protected:
+	motion_node(const point& a, const point& b) : a(a), b(b) {}
+	motion_node() {}
+	~motion_node() {}
 
+private:
+	point a;
+	point b;
+};
+
+class linear_motion : public motion_node {
+public:
+	linear_motion(point a, point b) : motion_node(a, b) {}
 	linear_motion() = default;
 };
 
-class hypocycloid_motion : public motion_node {
-
-	double radius;
-
-public:
-
-	hypocycloid_motion(const point& a, const point& b, double radius) 
-		: motion_node(), a(a), b(b), radius(radius) {}
-
-};
-
-class bezier_motion : public motion_node {
-	point p0;
-	point p1;
-	point p2;
-	point p3;
-
-public:
-
-	bezier_motion(const point& p0, const point& p1,
-		const point& p2, const point& p3)
-		: p0(p0), p1(p1), p2(p2), p3(p3) {}
-
-
-	bezier_motion() = default;
-};
-
-
 class profile {
-
 public:
-
-	profile(path_t& path) : path(path) {
+	profile(std::vector<point>* path) : path(path) {
 		linear_velocity = 0;
 		angular_velocity = 0;
 		cur_angle = 0;
 		tar_angle = 0;
+		at_destination = false;
+		m_path = nullptr;
 	};
 
-	double linear_velocity;
-
-	double angular_velocity;
-
-	double cur_angle;
-
-	double tar_angle;
-
-	path_t& path;
-
-	motion_path motion_path;
-
-	point current; 
-
-
-
-	std::pair<double, double> get_setspeed();
-
-	void compile_path_linear(std::vector<point_t> path);
-
-	void compile_path_hypocycloid(std::vector<point_t> path);
-
-	void compile_path_bezier(std::vector<point_t> path);
-
+	profile() : linear_velocity(0), angular_velocity(0), cur_angle(0), tar_angle(0), distance(0), at_destination(false), path(nullptr), m_path(nullptr) {};
 
 	void follow_path();
+	void setCurrent(double new_x, double new_y) { current = point(new_x, new_y); }
+	void setHeading(point target);
+	void setTargetAngle(double target);
+	void pointTurn();
+	void compile_path_linear(std::vector<point> path);
 
-		
+	std::pair<double, double> get_speed();
+
 private:
+	double linear_velocity;
+	double angular_velocity;
+	double cur_angle;
+	double tar_angle;
+	double distance;
+	double final_angle;
+	bool at_destination;
 
-	int v_max;
-
-	int v_min;
-
+	std::vector<point>* path;
+	std::vector<motion_node>* m_path;
+	point current; 
 };
--- src/ros/traversal/src/motion_profile.cpp
+++ src/ros/traversal/src/motion_profile.cpp
@@ -1,5 +1,5 @@
 #include "motion_profile.hpp"
-
+#define _USE_MATH_DEFINES
 #include <limits>	 //std::numeric_limits
 #include <algorithm> //std::min
 #include <stdexcept> //std::out_of_range, std::invalid_argument
@@ -14,30 +14,124 @@
 #include <memory>
 #include <iostream>
 #include <stdlib.h>  
+#include <cstdlib>
 
-using point = std::pair<uint16_t, uint16_t>;
+//using point = std::pair<uint16_t, uint16_t>;
 
 using namespace std;
 
+//------------Utility Functions----------
+double distance(point A, point B) {
+    return sqrt(pow(B.x - A.x, 2) + pow(B.y - A.y, 2));
+}
+
+//Point A is the current point,
+//Point B is the point along the line segment
+//Hypotenuse is the length of the 'stick'
+//Prefer left dictates whether or not the point C should be left or right of the line segment AB
+point findCarrot(point A, point B, double hypotenuse, bool prefer_left = false) {
+    // Calculate the distance between points A and B
+    double base_length = distance(A, B);
+
+    // Calculate the height of the right triangle (distance between point A and the line defined by B)
+    double height = sqrt(pow(hypotenuse, 2) - pow(base_length, 2));
+
+    // Calculate the direction from A to B
+    double dir_x = (B.x - A.x) / base_length;
+    double dir_y = (B.y - A.y) / base_length;
+
+    // Determine the perpendicular direction
+    double perp_dir_x = -dir_y; // Swap and negate one component to obtain a perpendicular vector
+    double perp_dir_y = dir_x;
+
+    // Determine the sign of the perpendicular direction based on the desired criteria
+    if (!prefer_left) {
+        perp_dir_x = -perp_dir_x;
+        perp_dir_y = -perp_dir_y;
+    }
+
+    // Calculate the coordinates of point C by moving along the perpendicular direction
+    point C;
+    C.x = B.x + height * perp_dir_x; // Perpendicular direction
+    C.y = B.y + height * perp_dir_y; // Perpendicular direction
+
+    return C;
+}
+
+//Use this function when the current point and the closest point to the curve are the same point
+//points A and B form a line segment
+//point C is the current point (
+point findCarrotSamePoint(point A, point B, point C, double distance_CD, bool prefer_left = true) {
+    // Calculate the vector AB
+    double vec_AB_x = B.x - A.x;
+    double vec_AB_y = B.y - A.y;
+
+    // Calculate the length of vector AB
+    double length_AB = distance(A, B);
+
+    // Normalize the vector AB to get the unit vector
+    double unit_AB_x = vec_AB_x / length_AB;
+    double unit_AB_y = vec_AB_y / length_AB;
+
+    // Determine the sign of the direction based on the desired criteria
+    double direction = prefer_left ? 1 : -1;
+
+    // Scale the unit vector AB by the distance CD to get the vector from C to D
+    double vec_CD_x = unit_AB_x * direction * distance_CD;
+    double vec_CD_y = unit_AB_y * direction * distance_CD;
+
+    // Calculate the coordinates of point D by adding the vector CD to point C
+    point D;
+    D.x = C.x + vec_CD_x;
+    D.y = C.y + vec_CD_y;
+
+    // Check if point D lies outside the segment AB
+    if ((D.x - A.x) * (D.x - B.x) > 0 || (D.y - A.y) * (D.y - B.y) > 0) {
+        // If D is outside the segment, set it to the closest endpoint of the segment
+        double dist_to_A = distance(C, A);
+        double dist_to_B = distance(C, B);
+        if (dist_to_A < dist_to_B) {
+            D = A;
+        }
+        else {
+            D = B;
+        }
+    }
+
+    return D;
+}
 
 //------------Motion Profile-------------
-profile::profile() {
-	linear_velocity = 0;
-	angular_velocity = 0;
-	heading = 0;
-
-};
 
 //Compile the angular and linear velocity of the profile to form percentages for each tracks
 //Should return a pair of integers
-std::pair<double, double> profile::get_setspeed() {
-	return 0;
+std::pair<double, double> profile::get_speed() {
+	return p(0.0, 0.0);
 };
 
 
 
 
-//follow path
+//-------------Profile methods-------------
+
+void profile::compile_path_linear(std::vector<point> path) {
+    if (this->path == nullptr) return;
+
+    if (path.size() < 2) {
+        std::cerr << "Path must contain at least 2 points for compilation." << std::endl;
+        return;
+    }
+
+    this->motion_path.clear();
+
+    for (size_t i = 0; i < path.size() - 1; ++i) {
+        this->motion_path->push_back(std::make_shared<linear_motion>(path[i], path[i + 1]));
+    }
+
+    return;
+
+}
+
 /*
 	Take the given node vector, consider it only one node at a time?
 	Find the point on the path where the current position is closest to
@@ -49,125 +143,113 @@ std::pair<double, double> profile::get_setspeed() {
 */
 
 void profile::follow_path() {
-	using path_t = std::vector<point>;
+    if (this.path == nullptr) return;
+    if (this.m_path == nullptr) return;
 
-	using motion_path = std::vector<motion_node>;
+	motion_node curnode = m_path->front();
 
-	motion_path curpath = motion_path.front();
+    //Get the carrot point
 
-	//Get update all local variables
-
-	point closest_point = curpath.get_closest_point(this->current);
-    //if the closest point is beyond the boundaries, then move to the next spot on the path;
-    //Perhaps we can decide that the boundary is a radius around the point
-
-
-
-	//if the closest point is the target 
-
-	this->tar_angle = curpath.get_tangent_angle(closest_point);
+    point target = curnode.get_target_from_distance(current, distance); //given the distance get the target
     
+    //compare the current point to the target point
+    //If the front of our path vector is also the end, then we don't want to do all this stuff because the last point is our final target
+    if (curnode.get_end() == target && m_path->front() != m_path.->end()) {
+        //Erase the first path from m_path
+        m_path->erase(m_path->front());
 
-	//compare tar angle to current angle, then adjust the linear & angular velocity based on the difference
-    double velocity = abs(cur_angle - tar_angle);
+        curnode = m_path->front();
 
-    this->linear_velocity = 0.8;
-    this->angular_velocity = 0.0;
+        target = curnode.get_target_from_distance(current, distance);
+    }
+    
+    //Assumes a normalized target angle
+	//if the closest point is the target 
+    this->cur_angle = setHeading(target);
 
-	//send the 'get_velocity' function to wherever sends the velocity
+    double headings[2] = { (tar_angle - 360 - cur_angle), (tar_angle + 360 - cur_angle) };
+
+    double shortestDiff = abs(tar_angle - current);
+
+    for (int i = 0; i < 2; i++) {
+       double t = abs(headings[i]);
+        if (t < shortestDiff) shortestDiff = t;
+    }
+
+    //Get the actual distance away from the target. Useful for final approach, the 'stick' length is broken
+    
+    double actual_distance = distance(this->current, target);
+	//compare shortest target angle to the current angle, then adjust the linear & angular velocity based on the difference
+    //If the difference in degrees is greater than 30, then do a point turn (linear velocity is zero)
+    
+    double percentage = abs(shortestDiff / 360);
+    if (shortestDiff < 0) percentage = percentage * -1;
+
+    if (!at_destination) {
+        if (shortestDiff > 30) {
+                this->linear_velocity = 0.0;
+                this->angular_velocity = percentage;
+            }
+            else {
+                this->linear_velocity = (1 - percentage); //If we are at our current heading, go 100%
+                this->angular_velocity = percentage;
+            }
+    }
+    else {
+        //If we are finished, then this method should not be setting the velocities to anything.
+        linear_velocity = 0.0;
+        angular_velocity = 0.0;
+    }
+    
+   
+    return;
+}
 
 
-	
+void profile::setHeading(point target) {
+    // Calculate the differences in x and y coordinates
+    double dx = target.x - current.x;
+    double dy = target.y - current.y;
+
+    // Calculate the angle (in radians) using arctan2
+    double angle_rad = std::atan2(dy, dx);
+
+    // Convert the angle from radians to degrees
+    double angle_deg = angle_rad * 180 / M_PI;
 
 
+    this->setTargetAngle(angle_deg);
 }
 
 
 
-//-------------Motion Structs-------------
-
-void profile::compile_path_linear(std::vector<point_t> path) {
-
-    if (path.size() < 2) {
-        std::cerr << "Path must contain at least 2 points for compilation." << std::endl;
-        return;
+void profile::setTargetAngle(double target)
+{
+    double normal = target % 360;
+    if (normal < 0) {
+        normal += 360;
     }
 
-    motion_path.clear();
-
-    for (size_t i = 0; i < path.size() - 1; ++i) {
-        motion_path.push_back(std::make_shared<linear_motion>(path[i], path[i + 1]));
-    }
-
-
+    tar_angle = normal;
 }
 
 
-void profile::compile_path_hypocycloid(std::vector<point_t> path) {
-    if (path.size() < 2) {
-        std::cerr << "Path must contain at least 2 points for compilation." << std::endl;
-        return;
+
+void profile::pointTurn() {
+    double headings[2] = { (final_angle - 360 - cur_angle), (final_angle + 360 - cur_angle) };
+
+    double shortestDiff = abs(final_angle - current);
+
+    for (int i = 0; i < 2; i++) {
+        double t = abs(headings[i]);
+        if (t < shortestDiff) shortestDiff = t;
     }
 
-    motion_path.clear();
+    double percentage = abs(shortestDiff / 360);
+    if (shortestDiff < 0) percentage = percentage * -1;
 
-    // Generate linear motion path
-    for (size_t i = 0; i < path.size() - 1; ++i) {
-        motion_path.push_back(std::make_shared<linear_motion>(path[i], path[i + 1]));
-    }
-
-    // Calculate and insert hypocycloidal motion nodes between linear motion nodes
-    for (size_t i = 0; i < motion_path.size() - 1; ++i) {
-        auto linear_node_a = std::dynamic_pointer_cast<linear_motion>(motion_path[i]);
-        auto linear_node_b = std::dynamic_pointer_cast<linear_motion>(motion_path[i + 1]);
-        if (linear_node_a && linear_node_b) {
-            double radius = 0.5 * std::sqrt(std::pow(path[i + 1].x - path[i].x, 2) + std::pow(path[i + 1].y - path[i].y, 2));
-            motion_path.insert(motion_path.begin() + i + 1, std::make_shared<hypocycloid_motion>(path[i], path[i + 1], radius));
-        }
-    }
-
-    // Recompile the path to ensure smooth transition between nodes
-    std::vector<point> compiled_path;
-    for (const auto& node : motion_path) {
-        compiled_path.push_back(node->a);
-    }
-    compiled_path.push_back(motion_path.back()->b);
-
-
-}
-
-void profile::compile_path_bezier(std::vector<point_t> path) {
-    if (path.size() < 2) {
-        std::cerr << "Path must contain at least 2 points for compilation." << std::endl;
-        return;
-    }
-
-    motion_path.clear();
-
-    // Generate linear motion path
-    for (size_t i = 0; i < path.size() - 1; ++i) {
-        motion_path.push_back(std::make_shared<linear_motion>(path[i], path[i + 1]));
-    }
-
-    // Calculate and insert Bezier curve motion nodes between linear motion nodes
-    for (size_t i = 0; i < motion_path.size() - 1; ++i) {
-        auto linear_node_a = std::dynamic_pointer_cast<linear_motion>(motion_path[i]);
-        auto linear_node_b = std::dynamic_pointer_cast<linear_motion>(motion_path[i + 1]);
-        if (linear_node_a && linear_node_b) {
-            // Calculate control points for Bezier curve
-            point p0 = linear_node_a->get_closest_point(path[i]);
-            point p3 = linear_node_b->get_closest_point(path[i + 1]);
-            point p1 = {
-                p0.x + (p3.x - p0.x) / 3.0,
-                p0.y + (p3.y - p0.y) / 3.0
-            };
-            point p2 = {
-                p3.x - (p3.x - p0.x) / 3.0,
-                p3.y - (p3.y - p0.y) / 3.0
-            };
-            motion_path.insert(motion_path.begin() + i + 1, std::make_shared<bezier_motion>(p0, p1, p2, p3));
-        }
-    }
+    this->linear_velocity = 0.0;
+    this->angular_velocity = percentage * ;
 }
 
 
@@ -180,14 +262,15 @@ double linear_motion::get_tangent_angle(point p) {
     return atan2(b.y - a.y, b.x - a.x);
 }
 
+
 point linear_motion::get_closest_point(const point& current) override {
     double x_diff = b.x - a.x;
     double y_diff = b.y - a.y;
     double t = ((current.x - a.x) * x_diff + (current.y - a.y) * y_diff) / (x_diff * x_diff + y_diff * y_diff);
-    if (t < 0) {
+   /* if (t < 0) {
         return a;
-    }
-    else if (t > 1) {
+    }*/
+    if (t > 1) {
         return b;
     }
     else {
@@ -195,38 +278,21 @@ point linear_motion::get_closest_point(const point& current) override {
     }
 }
 
+//Gets the target point that the carrot should point to. 
+//When interpreting this information, if the returned point is the same as point 'b' from this segment,
+//     assume that the distance does not hold then Move to the next segment
+point linear_motion::get_target_from_distance(point current, double distance) {
+    point closest = get_closest_point(current);
+    
+    point c = { 0, 0 };
 
-//-------------Bezier Motion--------------
-double bezier_motion::get_tangent_angle(const point& p) const override {
-    // For simplicity, we'll use the tangent angle at the start point of the Bezier curve
-    return atan2(p1.y - p0.y, p1.x - p0.x);
+    //Overloaded operator.
+    if (current == closest) {
+        c = findCarrotSamePoint(this.a, this.b, closest, distance, true);
+    }
+    else {
+        c = findCarrot(current, b, distance);
+    }
+
+    return c;
 }
-
-point bezier_motion::get_closest_point(const point& current) const override {
-    // Not implemented for Bezier curves
-    return p0; // Placeholder
-}
-
-
-//-------------Cycloid Motion-------------
-
-
-
-double hypocycloid_motion::get_tangent_angle(const point& p)  {
-    double angle = atan2(b.y - a.y, b.x - a.x);
-    double theta = atan2(p.y - a.y, p.x - a.x);
-    double diff = angle - theta;
-    while (diff > M_PI) diff -= 2 * M_PI;
-    while (diff < -M_PI) diff += 2 * M_PI;
-    return angle + diff / 2;
-}
-
-point hypocycloid_motion::get_closest_point(const point& current) {
-    double distance = sqrt((current.x - a.x) * (current.x - a.x) + (current.y - a.y) * (current.y - a.y));
-    double theta = atan2(b.y - a.y, b.x - a.x);
-    double t = distance / radius;
-    double x = a.x + radius * cos(theta + t);
-    double y = a.y + radius * sin(theta + t);
-    return point{x, y};
-}
-
