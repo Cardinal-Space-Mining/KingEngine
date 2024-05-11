53a0466fac2050e131f9a692eb513ec0bf1d4aef
Connor Tynan <connortynan16@gmail.com>
2024-05-11 00:06:19 -0500

    add toggleable barrier between nav zone and offload zone

--- src/ros/path_plan/src/path_plan/pathplan.hpp
+++ src/ros/path_plan/src/path_plan/pathplan.hpp
@@ -3,6 +3,7 @@
 #include "mapping.hpp"
 
 #include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/bool.hpp"
 #include "nav_msgs/msg/occupancy_grid.hpp"
 #include "nav_msgs/msg/path.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
@@ -22,6 +23,8 @@ protected:
 	void location_change_cb(const geometry_msgs::msg::PoseStamped &msg);
 	/** Called when target pose gets updated */
 	void destination_change_cb(const geometry_msgs::msg::PoseStamped &msg);
+    /** Called when changing whether we should avoid crossing from the navigation zone to construction zone */
+    void avoid_zone_flag_change_cb(const std_msgs::msg::Bool &flag);
 
 	/** Rerun navigation through the currently stored map and export the resulting path */
 	void export_data();
@@ -33,6 +36,10 @@ public:
 	static constexpr double DEFAULT_ROBOT_WIDTH = 0.8;
 	static constexpr int DEFAULT_TURN_COST = 0, DEFAULT_MIN_WEIGHT = 25;
 	static constexpr double DEFAULT_UPDATE_TIME_S = 0.5;
+
+    static constexpr double DEFAULT_AVOIDANCE_CORNER_X = 3.88;
+    static constexpr double DEFAULT_AVOIDANCE_CORNER_Y = 3.00;
+
 	static const std::string DEFAULT_OUTPUT_FRAME_ID;
 
 	static const std::string ROBOT_WIDTH_PARAM_NAME;
@@ -40,6 +47,8 @@ public:
 	static const std::string MIN_WEIGHT_PARAM_NAME;
 	static const std::string UPDATE_TIME_PARAM_NAME;
 	static const std::string OUTPUT_FRAME_PARAM_NAME;
+    static const std::string AVOID_ZONE_X_PARAM_NAME;
+    static const std::string AVOID_ZONE_Y_PARAM_NAME;
 
 	// Constant state
 protected:
@@ -48,12 +57,14 @@ protected:
 	const rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr lidar_data_sub;
 	const rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr dest_sub;
 	const rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr location_sub;
+    const rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr avoidance_zone_flag_sub;
 
 	const rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr path_pub;
 	const rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr weight_map_pub;
 
 	const float robot_width;
 	const int turn_cost, min_weight;
+    const float avoidance_barrier_x, avoidance_barrier_y;
 
 	const std::string output_frame_id;
 
@@ -63,8 +74,10 @@ protected:
 protected:
 	NavMap<int64_t, float> nav_map;
 
-	nav_msgs::msg::OccupancyGrid weights;
+    nav_msgs::msg::OccupancyGrid weights;
 	geometry_msgs::msg::Pose current_pose, target_pose;
+
+    bool include_avoidance_zone = false;
 
 	bool new_dst = false;
 	bool new_map_data = false;
