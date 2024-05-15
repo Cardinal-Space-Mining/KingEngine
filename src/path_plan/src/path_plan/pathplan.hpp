84299ffe700f402ecea65fc79f994a0f59b2a4da
Connor Tynan <connortynan16@gmail.com>
2024-05-14 21:26:18 -0500

    crop obstacle map to arena sized map

--- src/path_plan/src/path_plan/pathplan.hpp
+++ src/path_plan/src/path_plan/pathplan.hpp
@@ -10,6 +10,8 @@
 #include "geometry_msgs/msg/pose.hpp"
 #include "std_msgs/msg/float32.hpp"
 
+#include <opencv2/core.hpp>
+
 /** This class represents the path plannig ROS node (duh) */
 class PathPlanNode : public rclcpp::Node
 {
@@ -19,13 +21,21 @@ public:
 
 protected:
 	/** Called when obstacle grid gets updated */
-	void lidar_change_cb(const nav_msgs::msg::OccupancyGrid &map);
+	void lidar_change_cb(const nav_msgs::msg::OccupancyGrid &_map);
 	/** Called when robot pose from localization gets updated */
 	void location_change_cb(const geometry_msgs::msg::PoseStamped &msg);
 	/** Called when target pose gets updated */
 	void destination_change_cb(const geometry_msgs::msg::PoseStamped &msg);
     /** Called when changing whether we should avoid crossing from the navigation zone to construction zone */
     void avoid_zone_flag_change_cb(const std_msgs::msg::Bool &flag);
+
+    cv::Mat crop_to_arena_size(
+            const cv::Mat data,
+            const int map_w, const int map_h,
+            const int arena_min_x, const int arena_min_y,
+            const int arena_width, const int arena_height);
+
+    void globulize();
 
 	/** Rerun navigation through the currently stored map and export the resulting path */
 	void export_data();
@@ -41,6 +51,8 @@ public:
     static constexpr double DEFAULT_AVOIDANCE_CORNER_X = 3.88;
     static constexpr double DEFAULT_AVOIDANCE_CORNER_Y = 3.00;
     static constexpr int DEFAULT_AVOIDANCE_THICKNESS = 2;
+    static constexpr double DEFAULT_ARENA_WIDTH = 6.88;
+    static constexpr double DEFAULT_ARENA_HEIGHT = 5.00;
 
 	static const std::string DEFAULT_OUTPUT_FRAME_ID;
 
@@ -52,6 +64,8 @@ public:
     static const std::string AVOID_ZONE_X_PARAM_NAME;
     static const std::string AVOID_ZONE_Y_PARAM_NAME;
 	static const std::string AVOID_ZONE_THICKNESS_PARAM_NAME;
+    static const std::string ARENA_WIDTH_PARAM_NAME;
+    static const std::string ARENA_HEIGHT_PARAM_NAME;
 
 	// Constant state
 protected:
@@ -70,6 +84,7 @@ protected:
     const int turn_cost, min_weight;
 	const float avoidance_barrier_x, avoidance_barrier_y;
 	const int avoidance_barrier_thickness;
+    const double arena_width, arena_height;
 
 	const std::string output_frame_id;
 
@@ -79,13 +94,12 @@ protected:
 protected:
 	NavMap<int64_t, float> nav_map;
 
-    nav_msgs::msg::OccupancyGrid weights;
+    nav_msgs::msg::OccupancyGrid input_weights, weights;
 	geometry_msgs::msg::Pose current_pose, target_pose;
 
     bool include_avoidance_zone = false;
 
 	bool new_dst = false;
 	bool new_map_data = false;
-
 
 };
